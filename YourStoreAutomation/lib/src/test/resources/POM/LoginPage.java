a64446bef9aa03d4c6096e8cbc3bbef711926c77
ZahariaAlex <zaharia.alexandru.catalin@gmail.com>
2022-05-18 15:37:27 +0300

    update

--- YourStoreAutomation/lib/src/test/resources/POM/LoginPage.java
+++ YourStoreAutomation/lib/src/test/resources/POM/LoginPage.java
@@ -1,17 +1,23 @@
 package POM;
 
 
+import java.time.Duration;
+
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.FindBy;
 import org.openqa.selenium.support.How;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
 
 public class LoginPage {
 	WebDriver driver;
+	WebDriverWait wait;
 	
 	public LoginPage(WebDriver driver) {
 		this.driver = driver;
 	}
+	
 	
 	@FindBy(how = How.XPATH, using = "//a[@class = 'dropdown-toggle']//span[contains(text(), 'My Account')]")
 	private WebElement myAccountOption;
@@ -26,7 +32,7 @@ public class LoginPage {
 	private WebElement txtPwd;
 	
 	@FindBy(how = How.XPATH, using = "//input[@class = 'btn btn-primary']")
-	private WebElement loginBtw;
+	private WebElement loginBtn;
 	
 	@FindBy(how = How.XPATH, using = "//div[@class = 'form-group']//a[contains(text(), 'Forgotten Password')]")
 	private WebElement forgottenPwd;
@@ -39,6 +45,69 @@ public class LoginPage {
 	
 	@FindBy(how = How.XPATH, using = "//div[@class = 'pull-left']//a[contains(text(), 'Back')]")
 	private WebElement backBtn;
+	
+	@FindBy(how = How.XPATH, using = "//*[contains(text(), ' Warning: No match for E-Mail Address and/or Password.')]")
+	private WebElement messageWrongLogin;
+	
+	@FindBy(how = How.XPATH, using = "//*[contains(text(), ' Warning: The E-Mail Address was not found in our records, please try again!')]")
+	private WebElement messageWrongForgotten;
+	
+	
+	public void clickMyaccount() {
+		myAccountOption.click();
+	}
+	
+	public void clickLoginOption() {
+		loginOption.click();
+	}
+	
+	
+	public void txtEmail(String email) {
+		txtEmail.clear();
+		txtEmail.sendKeys(email);
+	}
+	
+	public void txtPwd(String pwd) {
+		txtPwd.clear();
+		txtPwd.sendKeys(pwd);
+	}
+	
+	public void loginBtn() {
+		loginBtn.click();
+	}
+	
+	
+	public void forgottenPwd() {
+		forgottenPwd.click();
+	}
+	
+	public void txtForgottenEmail(String email) {
+		txtforggottenEmail.clear();
+		txtforggottenEmail.sendKeys(email);
+	}
+	
+	
+	public void confirmBtn() {
+		confirmBtn.click();
+	}
+	
+	public void backBtn() {
+		backBtn.click();
+	}
+	
+	
+	public String messageWrongLogin() {
+		wait = new WebDriverWait(driver, Duration.ofSeconds(10));
+		wait.until(ExpectedConditions.visibilityOf(messageWrongLogin));
+		return messageWrongLogin.getText();	
+	}
+	
+	
+	public String messageWrongForgotten() {
+		wait = new WebDriverWait(driver, Duration.ofSeconds(10));
+		wait.until(ExpectedConditions.visibilityOf(messageWrongForgotten));
+		return messageWrongForgotten.getText();	
+	}
 	
 	
 	
--- YourStoreAutomation/lib/src/test/resources/pageLinks/PageLinks.java
+++ YourStoreAutomation/lib/src/test/resources/pageLinks/PageLinks.java
@@ -1,5 +1,31 @@
 package PageLinks;
 
 public enum PageLinks {
+	
+	
+	yourStoreLink("http://siit.epizy.com/"),
+	loginPage("http://siit.epizy.com/index.php?route=account/login"),
+	forgottenPwdPage("http://siit.epizy.com/index.php?route=account/forgotten"),
+	registerPage("http://siit.epizy.com/index.php?route=account/register"),
+	WrongLogin("Warning: No match for E-Mail Address and/or Password."),
+	wrongForgottenEmail(" Warning: The E-Mail Address was not found in our records, please try again!");
+	
+	
+	
+	private final String link;
+	
+
+	PageLinks(String link) {
+		this.link = link;
+	}
+	
+	
+	public String getLink() {
+		return this.link;
+	}
+	
+	
+	
+	
 
 }
